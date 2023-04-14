d6dcc4fd4948156bd4282f1faea3c609dca4ec9e
soxiray <55649491+soxiray@users.noreply.github.com>
2023-04-14 10:37:48 +0800

    Create JWT.java

---
+++ JWT.java
@@ -0,0 +1,280 @@
+package com.leyou.common.auth.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+/**
+ * @author 黑马程序员
+ */
+public class RsaUtils {
+
+    private static final int DEFAULT_KEY_SIZE = 2048;
+    /**
+     * 从文件中读取公钥
+     *
+     * @param filename 公钥保存路径，相对于classpath
+     * @return 公钥对象
+     * @throws Exception
+     */
+    public static PublicKey getPublicKey(String filename) throws Exception {
+        byte[] bytes = readFile(filename);
+        return getPublicKey(bytes);
+    }
+
+    /**
+     * 从文件中读取密钥
+     *
+     * @param filename 私钥保存路径，相对于classpath
+     * @return 私钥对象
+     * @throws Exception
+     */
+    public static PrivateKey getPrivateKey(String filename) throws Exception {
+        byte[] bytes = readFile(filename);
+        return getPrivateKey(bytes);
+    }
+
+    /**
+     * 获取公钥
+     *
+     * @param bytes 公钥的字节形式
+     * @return
+     * @throws Exception
+     */
+    private static PublicKey getPublicKey(byte[] bytes) throws Exception {
+        bytes = Base64.getDecoder().decode(bytes);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(bytes);
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        return factory.generatePublic(spec);
+    }
+
+    /**
+     * 获取密钥
+     *
+     * @param bytes 私钥的字节形式
+     * @return
+     * @throws Exception
+     */
+    private static PrivateKey getPrivateKey(byte[] bytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        bytes = Base64.getDecoder().decode(bytes);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(bytes);
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        return factory.generatePrivate(spec);
+    }
+
+    /**
+     * 根据密文，生存rsa公钥和私钥,并写入指定文件
+     *
+     * @param publicKeyFilename  公钥文件路径
+     * @param privateKeyFilename 私钥文件路径
+     * @param secret             生成密钥的密文
+     */
+    public static void generateKey(String publicKeyFilename, String privateKeyFilename, String secret, int keySize) throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        SecureRandom secureRandom = new SecureRandom(secret.getBytes());
+        keyPairGenerator.initialize(Math.max(keySize, DEFAULT_KEY_SIZE), secureRandom);
+        KeyPair keyPair = keyPairGenerator.genKeyPair();
+        // 获取公钥并写出
+        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+        publicKeyBytes = Base64.getEncoder().encode(publicKeyBytes);
+        writeFile(publicKeyFilename, publicKeyBytes);
+        // 获取私钥并写出
+        byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
+        privateKeyBytes = Base64.getEncoder().encode(privateKeyBytes);
+        writeFile(privateKeyFilename, privateKeyBytes);
+    }
+
+    private static byte[] readFile(String fileName) throws Exception {
+        return Files.readAllBytes(new File(fileName).toPath());
+    }
+
+    private static void writeFile(String destPath, byte[] bytes) throws IOException {
+        File dest = new File(destPath);
+        if (!dest.exists()) {
+            dest.createNewFile();
+        }
+        Files.write(dest.toPath(), bytes);
+    }
+}
+
+
+package com.leyou.common.auth.utils;
+
+import org.junit.Test;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import static org.junit.Assert.*;
+
+public class RsaUtilsTest {
+
+    private String pubPath = "D:\\ly_guangzhou126\\software\\rsa_key\\jwt_key.pub";
+
+    private String priPath = "D:\\ly_guangzhou126\\software\\rsa_key\\jwt_key";
+
+    @Test
+    public void generateKey() throws Exception {
+        //参数一：给公钥指定路径和名称，参数二：给私钥指定路径和名称，参数三：盐，参数四：密钥大小
+        RsaUtils.generateKey(pubPath, priPath, "heima", 2048);
+    }
+
+    @Test
+    public void getKey() throws Exception {
+        PublicKey publicKey = RsaUtils.getPublicKey(pubPath);
+        System.out.println(publicKey);
+        PrivateKey privateKey = RsaUtils.getPrivateKey(priPath);
+        System.out.println(privateKey);
+    }
+}
+
+<dependency>
+    <groupId>io.jsonwebtoken</groupId>
+    <artifactId>jjwt-api</artifactId>
+    <version>0.10.5</version>
+</dependency>
+<dependency>
+    <groupId>io.jsonwebtoken</groupId>
+    <artifactId>jjwt-impl</artifactId>
+    <version>0.10.5</version>
+    <scope>runtime</scope>
+</dependency>
+<dependency>
+    <groupId>io.jsonwebtoken</groupId>
+    <artifactId>jjwt-jackson</artifactId>
+    <version>0.10.5</version>
+    <scope>runtime</scope>
+</dependency>
+
+<!-- 后面的jwt工具类能用上-->
+<dependency>
+            <groupId>joda-time</groupId>
+            <artifactId>joda-time</artifactId>
+            <version>2.10.2</version>
+        </dependency>
+
+
+package com.leyou.common.auth.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author 黑马程序员
+ */
+@Data
+public class Payload<T> {
+    private String id;
+    private T userInfo;
+    private Date expiration;
+}
+
+package com.leyou.common.auth.utils;
+
+import com.leyou.common.auth.pojo.Payload;
+import com.leyou.common.utils.JsonUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.joda.time.DateTime;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+import java.util.UUID;
+
+
+public class JwtUtils {
+
+    private static final String JWT_PAYLOAD_USER_KEY = "user";
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位分钟
+     * @return JWT
+     */
+    public static String generateTokenExpireInMinutes(Object userInfo, PrivateKey privateKey, int expire) {
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY, JsonUtils.toString(userInfo))
+                .setId(createJTI())
+                .setExpiration(DateTime.now().plusMinutes(expire).toDate())
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位秒
+     * @return JWT
+     */
+    public static String generateTokenExpireInSeconds(Object userInfo, PrivateKey privateKey, int expire) {
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY, JsonUtils.toString(userInfo))
+                .setId(createJTI())
+                .setExpiration(DateTime.now().plusSeconds(expire).toDate())
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    /**
+     * 公钥解析token
+     *
+     * @param token     用户请求中的token
+     * @param publicKey 公钥
+     * @return Jws<Claims>
+     */
+    private static Jws<Claims> parserToken(String token, PublicKey publicKey) {
+        return Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token);
+    }
+
+    private static String createJTI() {
+        return new String(Base64.getEncoder().encode(UUID.randomUUID().toString().getBytes()));
+    }
+
+    /**
+     * 获取token中的用户信息
+     *
+     * @param token     用户请求中的令牌
+     * @param publicKey 公钥
+     * @return 用户信息
+     */
+    public static <T> Payload<T> getInfoFromToken(String token, PublicKey publicKey, Class<T> userType) {
+        Jws<Claims> claimsJws = parserToken(token, publicKey);
+        Claims body = claimsJws.getBody();
+        Payload<T> claims = new Payload<>();
+        claims.setId(body.getId());
+        claims.setUserInfo(JsonUtils.toBean(body.get(JWT_PAYLOAD_USER_KEY).toString(), userType));
+        claims.setExpiration(body.getExpiration());
+        return claims;
+    }
+
+    /**
+     * 获取token中的载荷信息
+     *
+     * @param token     用户请求中的令牌
+     * @param publicKey 公钥
+     * @return 用户信息
+     */
+    public static <T> Payload<T> getInfoFromToken(String token, PublicKey publicKey) {
+        Jws<Claims> claimsJws = parserToken(token, publicKey);
+        Claims body = claimsJws.getBody();
+        Payload<T> claims = new Payload<>();
+        claims.setId(body.getId());
+        claims.setExpiration(body.getExpiration());
+        return claims;
+    }
+}
+
